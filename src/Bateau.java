b3f8d28fb6217395783c4fe831d0b515e5357c88
RomainNcls <VriziXLP@hotmail.com>
2019-03-19 17:50:53 +0100

    Update Bateau.java

--- src/Bateau.java
+++ src/Bateau.java
@@ -1,8 +1,59 @@
+/**
+ * @author romain
+ *
+ */
 public class Bateau {
 	@SuppressWarnings("unused")
 	private int vie;
+	private boolean mort;
+	private String nom;
+	private String dir;
+	private int taille;
 	
-	public Bateau (int v) {
-		this.vie = v;
+	
+	/**
+	 * @param vie
+	 * @param mort
+	 * @param nom
+	 * @param dir
+	 * @param taille
+	 */
+	public Bateau(int vie, boolean mort, String nom, String dir, int taille) {
+		super();
+		this.vie = vie;
+		this.mort = false;
+		this.nom = " ";
+		this.dir = dir;
+		this.taille = taille;
 	}
+	
+	/**
+	 * @return
+	 */
+	public boolean estMort() {
+		if (this.vie == 0) {
+			mort=true;
+			
+			
+			
+			
+		}else {
+			this.taille--;
+			mort=false;
+		
+		
+		}
+		return mort;
+		
+	}
+	
+	/**
+	 * @return
+	 */
+	public int getTaille() {
+		return this.taille;
+	}
+	
+	
+
 }
