d178b3aaa2d90dd8dba1f7f0612488299beb98fd
NUNZIO_CONTE2 <nunzio.conte2@etu.univ-lorraine.fr>
2019-03-30 19:03:56 +0100

    fix getVie bateau
    Devait retourné un réel et non un entier

--- src/Bateau.java
+++ src/Bateau.java
@@ -5,6 +5,38 @@
  */
 
 public class Bateau {
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Bateau))
+			return false;
+		Bateau other = (Bateau) obj;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		return true;
+	}
+
 	@SuppressWarnings("unused")
 	private String nom;
 	private int vie;
@@ -37,8 +69,8 @@ public class Bateau {
 	 * Donne la vie du Bateau en pourcentage
 	 * @return la vie en %
 	 */
-	public int getVie() {
-		return this.vie/this.taille*100;
+	public double getVie() {
+		return (double)this.vie/(double)this.taille*100.0 ;
 	}
 	
 	/**
@@ -51,5 +83,10 @@ public class Bateau {
 	
 	public void touche() {
 		this.vie--;
+	}
+	
+	
+	public String getNom() {
+		return nom;
 	}
 }
