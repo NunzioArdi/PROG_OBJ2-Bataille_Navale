3252fd7883631e8fc033b2433171c314da397ffa
NUNZIO_CONTE2 <nunzio.conte2@etu.univ-lorraine.fr>
2019-04-07 16:07:36 +0200

    Update CoordoneeException.java
    supression constructeur sans parametre

--- src/Exceptions/CoordoneeException.java
+++ src/Exceptions/CoordoneeException.java
@@ -6,10 +6,6 @@ public class CoordoneeException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 7457002118191721614L;
-
-	public CoordoneeException() {
-		super();
-	}
 	
 	public CoordoneeException(String msg) {
 		super(msg);
